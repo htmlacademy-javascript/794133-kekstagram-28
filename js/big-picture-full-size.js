809a7e31d351c5c8054f3b2d24424b221f5acca4
Ludmila1990 <lusuntsova@mail.ru>
2023-03-20 16:35:43 +0500

    feat: add load comments on click

--- js/big-picture-full-size.js
+++ js/big-picture-full-size.js
@@ -7,18 +7,11 @@ const socialCommentList = document.querySelector('.social__comments');
 const socialComment = socialCommentList.querySelector('.social__comment');
 const socialCommentCount = bigPictureModal.querySelector('.social__comment-count');
 const socialCaption = bigPictureModal.querySelector('.social__caption');
-const commentsLoader = bigPictureModal.querySelector('.comments-loader');
+const commentsLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
-
-// Закрытие фото с помощью кнопки Esc
-
-const onDocumentKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    // eslint-disable-next-line no-use-before-define
-    closeFullSizePhoto();
-  }
-};
+const COMMENTS_TO_SHOW_INITIAL = 5;
+let currentCommentsCount = COMMENTS_TO_SHOW_INITIAL;
+let currentPicture;
 
 // Открытие большого фото
 
@@ -28,39 +21,6 @@ const openFullSizePhoto = () => {
   socialCommentList.innerHTML = '';
 
   document.addEventListener('keydown', onDocumentKeydown);
-};
-
-// Создание комментария на большом фото
-
-const createCommentOnBigPhoto = (comment) => {
-  const commentElement = socialComment.cloneNode(true);
-  commentElement.querySelector('.social__picture').src = comment.avatar;
-  commentElement.querySelector('.social__picture').alt = comment.name;
-  commentElement.querySelector('.social__text').textContent = comment.message;
-  return commentElement;
-};
-
-// Воспроизведение комментариев на большом фото
-
-const renderComments = (comments) => {
-  const commentsFragment = document.createDocumentFragment();
-  comments.forEach((comment) => {
-    commentsFragment.append(createCommentOnBigPhoto(comment));
-  });
-  socialCommentList.append(commentsFragment);
-};
-
-// Воспроизведение большого фото
-
-const renderBigPictureFullScreen = (picture) => {
-  openFullSizePhoto();
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
-  bigPictureImg.src = picture.url;
-  likesCount.textContent = picture.likes;
-  socialCaption.textContent = picture.description;
-  commentsCount.textContent = picture.comments.length;
-  renderComments(picture.comments);
 };
 
 // Функция закрытия большого фото
@@ -77,5 +37,66 @@ const closeFullSizePhoto = () => {
 btnCloseBigPicture.addEventListener('click', () => {
   closeFullSizePhoto();
 });
+
+// Закрытие фото с помощью кнопки Esc
+
+function onDocumentKeydown (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+
+    closeFullSizePhoto();
+  }
+}
+
+// Создание комментария на большом фото
+
+const createCommentOnBigPhoto = (comment) => {
+  const commentElement = socialComment.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__picture').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  return commentElement;
+};
+
+// Воспроизведение комментариев на большом фото
+
+const renderComments = (comments) => {
+
+  const commentsFragment = document.createDocumentFragment();
+  comments.slice(0, currentCommentsCount).forEach((comment) => {
+    commentsFragment.append(createCommentOnBigPhoto(comment));
+  });
+  socialCommentList.innerHTML = '';
+  socialCommentList.append(commentsFragment);
+  if (currentCommentsCount >= comments.length) {
+    currentCommentsCount = comments.length;
+    commentsLoader.classList.add('hidden');
+    commentsLoader.removeEventListener('click', updateLoadMoreClick);
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+  socialCommentCount.textContent = `${currentCommentsCount} из ${comments.length} комментариев`;
+};
+
+// Обновление комментариев при клике
+
+function updateLoadMoreClick () {
+  currentCommentsCount += COMMENTS_TO_SHOW_INITIAL;
+  renderComments(currentPicture.comments);
+}
+
+// Воспроизведение большого фото
+
+const renderBigPictureFullScreen = (picture) => {
+  openFullSizePhoto();
+  currentPicture = picture;
+  currentCommentsCount = COMMENTS_TO_SHOW_INITIAL;
+  bigPictureImg.src = picture.url;
+  likesCount.textContent = picture.likes;
+  socialCaption.textContent = picture.description;
+  commentsCount.textContent = picture.comments.length;
+  commentsLoader.addEventListener('click', updateLoadMoreClick);
+  renderComments(picture.comments);
+};
 
 export {renderBigPictureFullScreen};
